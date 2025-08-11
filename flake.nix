c5222b7e9720d2eb1f248cb04bf6c8f93f664987
Neal Wang <nealwang.sh@protonmail.com>
2025-08-11 16:58:00 -0700

    feat: init tauri app

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Flowstate";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          gobject-introspection
+          cargo
+          cargo-tauri
+          nodejs
+        ];
+
+        buildInputs = with pkgs; [
+          at-spi2-atk
+          atkmm
+          cairo
+          gdk-pixbuf
+          glib
+          gtk3
+          harfbuzz
+          librsvg
+          libsoup_3
+          pango
+          webkitgtk_4_1
+          openssl
+        ];
+      };
+    };
+}
